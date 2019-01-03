4ecd59f60f904a5fce4ee0fee5acaaeb1960ef07
Egon Patrick <egon.16@hotmail.com>
2019-01-03 21:55:47 -0200

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
+//iniciando a sessão
+session_start();
 //adiciona os arquivos necessarios
 require_once("vendor/autoload.php");
 //cria uma nova aplicação do SLIM
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 //configura o modo debug para explicar cada erro
@@ -20,11 +23,45 @@ $app->get('/', function() {
 //criando rota administração
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
 });
+
+//criando rota login
+$app->get('/admin/login', function() {
+    // essa pagina nao tem header nem footer, entao precisa
+    //desabilitar essas opcoes
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->get('/admin/logout', function() {
+    
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
 //manda executar a aplicação
 $app->run();
 
